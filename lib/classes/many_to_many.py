44fe7dd03fed8f1b2a6ead8c6d4e09943091a105
m11toh <martohmashah@gmail.com>
2024-05-26 22:58:39 +0300

    data structures

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -5,20 +5,27 @@ class Article:
         self.title = title
         
 class Author:
-    def __init__(self, name):
-        self.name = name
+       def __init__(self, name):
+        self._name = name
 
-    def articles(self):
-        pass
+        @property
+        def name(self):
+            return self._name
 
-    def magazines(self):
-        pass
+        def articles(self):
+            return [article for article in Article.all_articles if article.author == self]
 
-    def add_article(self, magazine, title):
-        pass
+        def magazines(self):
+            return list(set(article.magazine for article in self.articles()))
 
-    def topic_areas(self):
-        pass
+        def add_article(self, magazine, title):
+            return Article(self, magazine, title)
+
+        def topic_areas(self):
+            articles = self.articles()
+            if not articles:
+                return None
+            return list(set(article.magazine.category for article in articles))
 
 class Magazine:
     def __init__(self, name, category):
